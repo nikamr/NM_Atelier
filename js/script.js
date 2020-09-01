e968d8973072eb502575ec22ded22dc93f5d7835
nikamr <nika.mrden@gmail.com>
2020-09-01 19:05:46 +0200

    Javascript
    -not working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Ciao creative soul, welocme to my portfolio page!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
